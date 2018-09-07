fc4ae6bbb60c818e108e48dea3167276b6f95348
Eduard Sergeev <eduard.sergeev@gmail.com>
2018-09-07 18:19:37 +1000

    Make it more responsive
    Better, dynamic layout on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,8 @@
 
 $(function() {
   $("#content h2, #content h3").not(".nav-exclude").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+    $("nav ul").append("<li class='nav-item tag-" + this.nodeName.toLowerCase() + "'><a class='nav-link' href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
@@ -13,18 +12,8 @@ $(function() {
     var scrollTop = position + scroll - 10;
     scrollTop = scrollTop < 70 ? 0 : scrollTop;
     $("#scrollable").animate({scrollTop: scrollTop}, 500);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
+    $('.collapse').collapse('hide');
     event.preventDefault();
   });
 
-  $("#scrollable").scrollspy({offset: 50});
-
-  // $("#scrollable").scrollspy({target: "nav", offset: 50});
-
-  
-  // $("#scrollable").on("scroll", function ()
-  // {
-  //   var scroll = $("#scrollable").scrollTop();
-  // });
 });
